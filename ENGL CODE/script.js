2ad9bf26d6e36f3445bf84b844aaab2690fa2671
carlososoriosv <carlososorioamaya@gmail.com>
2023-09-20 11:28:25 -0400

    Instructions ENGL393

---
+++ ENGL CODE/script.js
@@ -0,0 +1,13 @@
+$(function() {
+    new Typed('#typed', {
+          strings: ["RED","BLUE","PURPLE","PROGRAMMER"],
+          typeSpeed: 100,
+      backDelay: 1500,
+      loop: true,
+      callback: function(){}
+      });
+  });
+  
+
+  
+  
